9ea112ac9cfb13e7c32901aeff20cb95be595bc8
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-03-28 16:39:27 -0400

    complete nav link toggle color functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ const secondaryDark = "#594d9c";
 
 const body = document.querySelector("body");
 const nav = document.querySelector("nav");
+const navLinks = document.querySelectorAll(".nav-link");
 const header = document.querySelector("header");
 const aboutMe = document.querySelector(".about-me");
 const solidFill = document.querySelector(".solid-fill");
@@ -13,6 +14,8 @@ const stops = waveGradient.getElementsByTagName("stop");
 
 const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
+aboutMe.style.setProperty("--about-me-after-bg-color", primary);
+
 function darkMode() {
     body.classList.toggle("dark-body");
     nav.classList.toggle("dark-gradient");
@@ -20,12 +23,28 @@ function darkMode() {
     aboutMe.classList.toggle("dark-border-top");
 
     if(aboutMe.classList.contains('dark-border-top')) {
+        navLinks.forEach(link => {
+            link.addEventListener("mouseover", function() {
+                link.style.color = primaryDark;
+            })
+            link.addEventListener("mouseout", function() {
+                link.style.color = "";
+            });
+        });
         aboutMe.style.borderTop = "7px solid " + primaryDark;
         aboutMe.style.setProperty("--about-me-after-bg-color", primaryDark);
         solidFill.setAttribute("fill", secondaryDark);
         stops[0].setAttribute("stop-color", primaryDark);
         stops[1].setAttribute("stop-color", secondaryDark);
     } else {
+        navLinks.forEach(link => {
+            link.addEventListener("mouseover", function() {
+                link.style.color = primary;
+            })
+            link.addEventListener("mouseout", function() {
+                link.style.color = "";
+            });
+        });
         aboutMe.style.borderTop = "7px solid " + primary;
         aboutMe.style.setProperty("--about-me-after-bg-color", primary);
         solidFill.setAttribute("fill", secondary);
