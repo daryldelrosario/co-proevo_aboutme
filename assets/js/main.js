8d21b0b12825dccb1bf622e1dfd5f85818e920f7
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-05-08 18:12:14 -0400

    complete list sorter full function

--- assets/js/main.js
+++ assets/js/main.js
@@ -381,3 +381,80 @@ fname.on("input", () => removeFocusedStyle(0, fname));
 lname.on("input", () => removeFocusedStyle(1, lname));
 email.on("input", () => removeFocusedStyle(2, email));
 message.on("input", () => removeFocusedStyle(3, message));
+
+// ============================
+// FOR LIST SORTER - PROJECT 13
+// ============================
+// SETTING UP THE TABLE WITH PRESET DATA
+// FUNCTION: PRESET ROW
+function presetRow(fname, lname, age) {
+    const row = $("<tr>");
+    const fnameCell = $("<td>").text(fname);
+    const lnameCell = $("<td>").text(lname);
+    const ageCell = $("<td>").text(age);
+    row.append(fnameCell, lnameCell, ageCell);
+
+    $("#list-sorter-table tbody").append(row);
+}
+
+// Preset the table with some data
+presetRow("Daryl", "del Rosario", 36);
+presetRow("Clark Kent", "del Rosario", 1);
+presetRow("Ashley", "Martins Pimentel", 35);
+presetRow("Aqua", "Man", 10);
+presetRow("Cat", "Woman", 2);
+presetRow("Hello", "World", 20);
+
+// FUNCTION: ADD ROW
+function addRow() {
+    const firstName = $("#firstName").val();
+    const lastName = $("#lastName").val();
+    const age = $("#age").val();
+
+    const row = $("<tr>");
+    const fNameCell = $("<td>").text(firstName);
+    const lNameCell = $("<td>").text(lastName);
+    const ageCell = $("<td>").text(age);
+    row.append(fNameCell, lNameCell, ageCell);
+
+    $("#list-sorter-table tbody").append(row);
+
+    $("#firstName").val("");
+    $("#lastName").val("");
+    $("#age").val("");
+}
+
+// CREATING SORTING FUNCTION sortTable(a, b, c)
+// FUNCTION: SORT TABLE 
+function sortTable(table, columnIndex, isNumeric) {
+    const rows = table.find("tbody > tr").get();
+    rows.sort(function(a, b) {
+        const cellA = $(a).find("td").eq(columnIndex).text();
+        const cellB = $(b).find("td").eq(columnIndex).text();
+        if(isNumeric) {
+            return Number(cellA) - Number(cellB);
+        } else {
+            return cellA.localeCompare(cellB);
+        }
+    });
+
+    const currentOrder = $(table.find("th").get(columnIndex)).attr("data-order");
+    if(currentOrder === "asc") {
+        rows.reverse();
+        $(table.find("th").get(columnIndex)).attr("data-order", "desc");
+    } else {
+        $(table.find("th").get(columnIndex)).attr("data-order", "asc");
+    }
+
+    $.each(rows, function(index, row) {
+        table.children("tbody").append(row);
+    });
+}
+
+// Adding click event handlers to the table headers for sorting 
+$("#list-sorter-table th").click(function() {
+    const table = $(this).closest("table");
+    const columnIndex = $(this).index();
+    const isNumeric = columnIndex === 2;
+    sortTable(table, columnIndex, isNumeric);
+});
